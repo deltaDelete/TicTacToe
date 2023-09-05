6241d75ff9fa7df881624449d66ab8db25fb422f
deltaDelete <samansy@yandex.ru>
2023-09-05 21:39:55 +0700

    chore(AboutPage.kt): удалены ненужные импорты

--- app/src/main/java/ru/deltadelete/tictactoe/pages/AboutPage.kt
+++ app/src/main/java/ru/deltadelete/tictactoe/pages/AboutPage.kt
@@ -1,11 +1,9 @@
 package ru.deltadelete.tictactoe.pages
 
-import android.widget.ImageButton
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,16 +13,12 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.rememberAsyncImagePainter
-import ru.deltadelete.tictactoe.MainActivity
 import ru.deltadelete.tictactoe.R
 import ru.deltadelete.tictactoe.REDMI_NOTE_9_PRO
-import ru.deltadelete.tictactoe.ui.theme.TicTacToeTheme
 import ru.deltadelete.tictactoe.ui.theme.Typography
 
 @OptIn(ExperimentalMaterial3Api::class)
